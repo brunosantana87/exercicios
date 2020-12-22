cb6811423ee4db08969c40e67add678ce36a44b8
Bruno A. Santana <brunosantana87@gmail.com>
2020-12-22 15:55:37 -0300

    Alterações no PrimeiroPrograma.java

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -1,9 +1,39 @@
 package fundamentos;
 
+/**
+ * Esse é o JAVADOC
+ * 
+ * Essa classe representa...
+ * 
+ * @author Bruno A. Santana <bruno@email.com>
+ * @since 22/12/2020
+ * @version 1.0
+ *
+ */
+
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
+		
+		// Comentário: Uma sentença de código termina com ";"
 		System.out.println("Ola, mundo!!! Primeiro programa Parte #01!!!");
+		
+		/*
+		 * Exemplo de bloco de comentário
+		 * 
+		 * Linha 1
+		 * Linha 2
+		 * 
+		 * */
+		
+		/*
+		 * Mais um bloco de comentário
+		 */
+		
 		System.out.println("Ola, mundo!!! Primeiro programa Parte #02!!!");
+		
+		System.out.println("Fim"); // Aqui tb funciona o comentário
+		
+		
 	}
 }
