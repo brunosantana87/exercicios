c5e14d27da4c789c221b06858cb6a4f548e1e161
brunosantana87 <73925162+brunosantana87@users.noreply.github.com>
2020-12-22 00:30:13 -0300

    Update PrimeiroPrograma.java

--- src/fundamentos/PrimeiroPrograma.java
+++ src/fundamentos/PrimeiroPrograma.java
@@ -3,6 +3,7 @@ package fundamentos;
 public class PrimeiroPrograma {
 	
 	public static void main(String[] args) {
-		System.out.println("Ola, mundo!!! Primeiro programa!!!");
+		System.out.println("Ola, mundo!!! Primeiro programa Parte #01!!!");
+		System.out.println("Ola, mundo!!! Primeiro programa Parte #02!!!");
 	}
 }
